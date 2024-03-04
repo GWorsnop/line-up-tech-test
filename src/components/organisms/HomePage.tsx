407d745173c32d2fa4d5c176e1f5303f3271c6f5
George Worsnop <georgeworsnop@gmail.com>
2024-03-04 22:41:21 +0000

    fix(home): add in error handling for wrong page number

--- src/components/organisms/HomePage.tsx
+++ src/components/organisms/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import {
   StyledButton,
@@ -6,10 +6,12 @@ import {
   StyledGrid,
   StyledHomeContainer,
   StyledImage,
+  StyledImageContainer,
   StyledInfoContainer,
   StyledList,
 } from "./HomePage.style";
 import { useListUsersQuery } from "../../redux/userAPI";
+import ErrorPage from "../molecules/errorPage";
 
 type User = {
   id: number;
@@ -22,11 +24,7 @@ type User = {
 function Home() {
   const [searchParams, setSearchParams] = useSearchParams();
   const page = Number(searchParams.get("page") ?? 1);
-  console.log(page, "page");
-
   const { data: users, isLoading } = useListUsersQuery(page);
-  console.log(users, "data");
-  console.log(isLoading, "isLoading");
 
   useEffect(() => {
     const pageURL = searchParams.get("page");
@@ -41,39 +39,41 @@ function Home() {
         <p className="loader"></p>
       </div>
     );
-  } else
+  } else if (users?.data.length) {
     return (
       <StyledHomeContainer>
         <StyledGrid>
-          {users &&
-            users.data.map((user: User) => {
-              return (
-                <div key={user.id}>
+          {users.data.map((user: User) => {
+            return (
+              <div key={user.id}>
+                <StyledImageContainer>
                   <StyledImage
                     src={user.avatar}
                     alt={user.first_name}
                   ></StyledImage>
-                  <StyledInfoContainer>
-                    <StyledList>
-                      <dt>Name:</dt>
-                      <dd>
-                        {user.first_name} {user.last_name}
-                      </dd>
-                      <dt>Email:</dt>
-                      <dd>{user.email}</dd>
-                    </StyledList>
-                  </StyledInfoContainer>
-                  <StyledButtonContainer>
-                    <a href={`/user/${user.id}`}>
-                      <StyledButton>See more</StyledButton>
-                    </a>
-                  </StyledButtonContainer>
-                </div>
-              );
-            })}
+                </StyledImageContainer>
+                <StyledInfoContainer>
+                  <StyledList>
+                    <dt>Name:</dt>
+                    <dd>
+                      {user.first_name} {user.last_name}
+                    </dd>
+                    <dt>Email:</dt>
+                    <dd>{user.email}</dd>
+                  </StyledList>
+                </StyledInfoContainer>
+                <StyledButtonContainer>
+                  <a href={`/user/${user.id}`}>
+                    <StyledButton>See more</StyledButton>
+                  </a>
+                </StyledButtonContainer>
+              </div>
+            );
+          })}
         </StyledGrid>
       </StyledHomeContainer>
     );
+  } else return <ErrorPage />;
 }
 
 export default Home;
