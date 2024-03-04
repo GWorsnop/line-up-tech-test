81b89aad48d2eb33676c43689360dc2c1438f38e
George Worsnop <georgeworsnop@gmail.com>
2024-03-04 19:20:55 +0000

    feat(userpage): create user page and include params for redux

---
+++ src/components/organisms/UserPage.style.tsx
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+export const StyledUserContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 100px;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: center;
+    padding-top: 150px;
+  }
+`;
+
+export const StyledImage = styled.img`
+  height: 200px;
+  width: 200px;
+  border-radius: 100%;
+
+  @media (min-width: 768px) {
+    height: 250px;
+    width: 250px;
+  }
+`;
+
+export const StyledInfoContainer = styled.div`
+  padding-top: 30px;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    padding-left: 40px;
+    text-align: left;
+  }
+`;
+
+export const StyledUserName = styled.dd`
+  font-size: 32px;
+  font-weight: bold;
+
+  @media (min-width: 768px) {
+    font-size: 48px;
+  }
+`;
+
+export const StyledUserEmail = styled.div`
+  display: flex;
+  width: 100%;
+  font-size: 24px;
+  padding: 32px 8px;
+
+  @media (min-width: 768px) {
+    justify-content: flex-start;
+  }
+`;
+
+export const ExtraDetailsContainer = styled.div`
+  padding-top: 40px;
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    padding: 80px 20px 0;
+  }
+
+  div {
+    min-width: 50%;
+    padding: 20px;
+  }
+
+  h3 {
+    padding-bottom: 10px;
+    font-size: 22px;
+  }
+`;
---
+++ src/components/organisms/UserPage.tsx
@@ -0,0 +1,79 @@
+import { useParams } from "react-router-dom";
+import { useGetUserQuery } from "../../redux/userAPI";
+import {
+  StyledUserContainer,
+  StyledUserEmail,
+  StyledUserName,
+  StyledImage,
+  StyledInfoContainer,
+  ExtraDetailsContainer,
+} from "./UserPage.style";
+import ErrorPage from "../molecules/errorPage";
+
+function UserPage() {
+  const { userId } = useParams();
+
+  console.log(userId, "userId");
+
+  const { data: user, isLoading } = useGetUserQuery(Number(userId));
+  console.log(user, "data");
+  console.log(isLoading, "isLoading");
+
+  if (isLoading) {
+    return (
+      <div>
+        <p className="loader"></p>
+      </div>
+    );
+  } else if (user) {
+    return (
+      <>
+        <StyledUserContainer>
+          <StyledImage
+            src={user.data.avatar}
+            alt={user.data.first_name}
+          ></StyledImage>
+          <StyledInfoContainer>
+            <StyledUserName>
+              {user.data.first_name} {user.data.last_name}
+            </StyledUserName>
+            <StyledUserEmail>
+              <dd>{user.data.email}</dd>
+            </StyledUserEmail>
+          </StyledInfoContainer>
+        </StyledUserContainer>
+        <ExtraDetailsContainer>
+          <div>
+            <h3 className="pb-4 text-center text-3xl font-semibold md:pl-4 md:text-left">
+              Details:
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              suscipit aliquet augue, sed pretium sapien lacinia sit amet.
+              Praesent id ligula nisi. Duis iaculis, metus et laoreet feugiat,
+              neque tortor hendrerit turpis, non dictum velit turpis cursus
+              libero. In sit amet feugiat dolor, vitae mollis orci. In sodales
+              sapien at fringilla scelerisque. Etiam luctus dolor erat, quis
+              maximus ligula tempor eget. Ut auctor felis ut felis consectetur
+              dapibus.
+            </p>
+          </div>
+          <div>
+            <h3 className="pb-4 text-center text-3xl font-semibold md:pl-4 md:text-left">
+              Extra Details:
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et
+              est augue. Interdum et malesuada fames ac ante ipsum primis in
+              faucibus. Duis eu neque ex. Vestibulum suscipit sem sed felis
+              facilisis, vel tincidunt nibh rutrum. Pellentesque habitant morbi
+              tristique senectus et netus et malesuada fames ac turpis egestas.
+              Donec id enim rutrum orci egestas placerat ut eget ante.
+            </p>
+          </div>
+        </ExtraDetailsContainer>
+      </>
+    );
+  } else return <ErrorPage />;
+}
+export default UserPage;
