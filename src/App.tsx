9f8f4c80134953845b934203924dee2c776e5c5a
George Worsnop <georgeworsnop@gmail.com>
2024-03-04 19:18:28 +0000

    feat(router): add routes to the app

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Header from "./components/molecules/Header";
 import GlobalStyles from "./components/styles/Global.style";
 import { ThemeProvider } from "styled-components";
 import { Container } from "./components/styles/Container.style";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./components/molecules/errorPage";
+import UserPage from "./components/organisms/UserPage";
 
 // Theme for the general styling of the app
 const theme = {
@@ -11,10 +14,24 @@ const theme = {
     white: "#FFFFFF",
     grey: "#F6F8FB",
     cyan: "#399BA2",
+    lightCyan: "#4AB8BF",
     blue: "##012B4F",
     black: "#121212",
   },
 };
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/user/:userId",
+    element: <UserPage />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 function App() {
   return (
@@ -23,7 +40,7 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <Home />
+          <RouterProvider router={router} />
         </Container>
       </>
     </ThemeProvider>
