032818be37062007787f9766c794324cee1c4b75
George Worsnop <georgeworsnop@gmail.com>
2024-03-04 12:14:26 +0000

    fix(header): add styling to header to fix at top of page

--- src/components/molecules/Header.style.tsx
+++ src/components/molecules/Header.style.tsx
@@ -1,8 +1,15 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-  background-color: ${({ theme }) => theme.colors.header};
-  padding: 40px 0;
   display: flex;
+  position: fixed;
+  z-index: 10;
+  flex-direction: row;
+  height: 80px;
+  max-width: 100%;
+  width: 100%;
+  align-items: center;
   justify-content: center;
+  background-color: ${({ theme }) => theme.colors.grey};
+  opacity: 0.9;
 `;
