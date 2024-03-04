38234756cd0e30e6af99d32c438f39967ad7e529
George Worsnop <georgeworsnop@gmail.com>
2024-03-04 10:55:30 +0000

    feat(header): create simple header and add basic style

---
+++ src/components/molecules/Header.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  background-color: #ebfbff;
+  padding: 40px 0;
+  display: flex;
+  justify-content: center;
+
+  h1 {
+    color: black;
+  }
+`;
